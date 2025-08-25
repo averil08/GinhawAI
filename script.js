db2cace0aabb74503f54c20ecf6ab2df0b3901d9
davidreann <rafaaadavid@gmail.com>
2025-08-25 16:55:20 +0800

    Created script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("aboutBtn").addEventListener("click", function() {
+  window.location.href = "about.html";
+});
+
+document.getElementById("registerBtn").addEventListener("click", function() {
+  alert("Faculty registration popup will appear here.");
+});
+
+document.getElementById("selfCheckBtn").addEventListener("click", function() {
+  window.location.href = "selfcheck.html";
+});
